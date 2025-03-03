2c907f1dcf83a243e1fa522c28f04b72c53f18fb
Bismark Abban <abbanb2@gmail.com>
2025-03-03 22:33:55 +0000

    Pin Streamlit and Rich to specific versions in requirements and setup

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ setup(
     packages=find_packages(),
     install_requires=[
         'openpyxl==3.1.2',
-        'streamlit>=1.24.0',
+        'streamlit==1.24.0',
         'pandas==2.0.3',
         'pathlib==1.0.1',
         'Pillow==10.0.0',
-        'rich>=13.9.4'
+        'rich==13.9.4'
     ],
-    python_requires='>=3.8'
+    python_requires='>=3.8,<3.13'
 ) 
